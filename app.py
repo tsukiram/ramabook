4b42e89a0c7ed8927e757442aa6a3722055019ff
tsukiram <weebdur@gmail.com>
2024-07-24 10:20:49 +0700

    add responsive comment

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
-from flask_socketio import SocketIO, emit
-import eventlet
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/ramabook.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-socketio = SocketIO(app, async_mode='eventlet')
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -35,35 +29,13 @@ def index():
     comments = Comment.query.order_by(Comment.id.desc()).all()
     return render_template('index.html', comments=comments, username=username)
 
-@app.route('/add_comment', methods=['POST'])
-def add_comment():
-    username = request.form['username']
-    text = request.form['comment']
-    if username and text:
-        new_comment = Comment(username=username, text=text)
-        db.session.add(new_comment)
-        db.session.commit()
-        emit('new_comment', {
-            'id': new_comment.id,
-            'username': new_comment.username,
-            'text': new_comment.text
-        }, broadcast=True)
-        return jsonify({
-            'id': new_comment.id,
-            'username': new_comment.username,
-            'text': new_comment.text
-        })
-    else:
-        return jsonify({'error': 'Both fields are required!'}), 400
-
 @app.route('/delete_comment/<int:comment_id>', methods=['POST'])
 def delete_comment(comment_id):
     comment = Comment.query.get_or_404(comment_id)
     db.session.delete(comment)
     db.session.commit()
-    emit('delete_comment', {'id': comment.id}, broadcast=True)
     return jsonify({'result': 'success'})
 
 if __name__ == '__main__':
     db.create_all()
-    socketio.run(app, debug=True)
+    app.run(debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,21 +1,4 @@
 $(document).ready(function() {
-    const socket = io();
-
-    socket.on('new_comment', function(data) {
-        const newComment = `
-            <div class="comment" id="comment-${data.id}">
-                <div class="comment-text">
-                    <strong>${data.username}</strong>: ${data.text}
-                </div>
-                <button class="delete-btn" data-id="${data.id}">🗑️</button>
-            </div>`;
-        $('#comments-list').prepend(newComment);
-    });
-
-    socket.on('delete_comment', function(data) {
-        $(`#comment-${data.id}`).remove();
-    });
-
     $('.delete-btn').on('click', function() {
         const commentId = $(this).data('id');
         $.ajax({
@@ -25,25 +8,6 @@ $(document).ready(function() {
                 if (response.result === 'success') {
                     $(`#comment-${commentId}`).remove();
                 }
-            }
-        });
-    });
-
-    $('#comment-form').on('submit', function(event) {
-        event.preventDefault();
-        const formData = $(this).serialize();
-        $.ajax({
-            url: '/add_comment',
-            type: 'POST',
-            data: formData,
-            success: function(response) {
-                if (!response.error) {
-                    $('input[name="username"]').val(response.username);
-                    $('textarea[name="comment"]').val('');
-                }
-            },
-            error: function(response) {
-                alert(response.responseJSON.error);
             }
         });
     });
